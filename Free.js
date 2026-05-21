90b521301be90f0b8c58eebcf19e1cf536641abb
hab153 <Habeebullahapaokagi8@gmail.com>
2026-05-21 06:13:48 +0100

    Fix formatting of session meta building code

--- Free.js
+++ Free.js
@@ -1861,7 +1861,8 @@ async function _runLeadGenPipeline(safeMessage, history, userProfile, onProgress
     // Apply CRITICAL OUTPUT QUANTITY RULE
     const leadsToReturn = _applyOutputQuantityRules(allVerifiedLeads, requestedCount);
 
-    // Build session meta    const _meta = {
+    // Build session meta    
+    const _meta = {
         tavilyUsed:         tavilyQuota.used,
         tavilyRemaining:    getTavilyRemaining(),
         openAiCalls:        openAiTracker.totalCallsThisSession,
