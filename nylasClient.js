939e82b194527e132633ab322ac5accd6c9590ea
hab153 <Habeebullahapaokagi8@gmail.com>
2026-07-08 17:46:44 +0100

    Upgrade Nylas client from v5 to v8

--- nylasClient.js
+++ nylasClient.js
@@ -1,9 +1,10 @@
 const Nylas = require('nylas');
 require('dotenv').config();
 
-// Initialize Nylas v5 Client
-Nylas.config({
+// Initialize Nylas v8 Client
+const nylas = new Nylas({
   apiKey: process.env.NYLAS_API_KEY,
+  apiUri: 'https://api.us.nylas.com', // Ensure this matches your region (us or eu)
 });
 
-module.exports = Nylas;
+module.exports = nylas;
