c9d6493f940e7b44234a304a9e589b1e4c9ab9e7
hab153 <Habeebullahapaokagi8@gmail.com>
2026-07-08 10:35:07 +0100

    Refactor Nylas initialization and export

--- nylasClient.js
+++ nylasClient.js
@@ -1,9 +1,9 @@
-const Nylas = require('nylas');
+const { Nylas } = require('nylas');
 require('dotenv').config();
 
-// Initialize Nylas with your v3 API Key
-Nylas.config({
+// Initialize Nylas v3 Client
+const nylas = new Nylas({
   apiKey: process.env.NYLAS_API_KEY,
 });
 
-module.exports = Nylas;
+module.exports = nylas;
